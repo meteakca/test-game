645e8ecd7aea3a557fdf100316f1a57de50bf1a2
Mete Akcaoglu <makcaoglu@georgiasouthern.edu>
2020-08-21 14:35:08 -0400

    initial build

---
+++ 4_so_say_we_all!/Assets/Music.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Music : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadFirstScene", 3);
+    }
+
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
